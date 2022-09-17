4a2710d7d6c165aef3ef55ff2fd2ad0bea708864
Emelike Adaeze <emelikepatience961@gmail.com>
2022-09-17 06:05:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Quote from './components/quote';
+export default function App() {
+
+  return(
+    <>
+    <Quote />
+    </>
+  )
+}
+
---
+++ src/components/quote.js
@@ -0,0 +1,47 @@
+import React from "react";
+import quoteData from "../quoteData.js";
+
+export default function Quote() {
+
+  const [quotesArray, setQuote] = React.useState({
+    randomText: "",
+    author: ""
+  })
+
+  const [quoteText, setQuoteText] = React.useState(quoteData)
+
+  function handleClick() {
+    const quotesArray = quoteData.data.quotes
+    const randomQuotes = Math.floor(Math.random() * quotesArray.length)
+    const text = quotesArray[randomQuotes].text
+    const author = quotesArray[randomQuotes].author
+    setQuote({
+      randomText: text,
+      author: author
+
+    })
+  }
+
+  return (
+    <>
+      <main>
+        <header>
+          <h1>Generate Quote</h1>
+        </header>
+
+        <div className="quote-box">
+          <blockquote className="quotes">{quotesArray.randomText}
+                    <p className="author">~{quotesArray.author}</p>
+
+          
+          </blockquote>
+        </div>
+
+        <div className='btn'>
+          <button onClick={handleClick}>Generate quote</button>
+        </div>
+
+      </main>
+    </>
+  )
+}
